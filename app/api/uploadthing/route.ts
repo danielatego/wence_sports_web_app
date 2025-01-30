887a787cad789563b8afda416b2d5520dc1dc529
danielatego <danielatego75@gmail.com>
2025-01-30 19:03:22 +0300

    token

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -2,6 +2,9 @@ import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
 export const {GET,POST} = createRouteHandler({
-    router:ourFileRouter
+    router:ourFileRouter,
+    config:{
+        token:'eyJhcGlLZXkiOiJza19saXZlX2FkZTAyYWI0YzI0YjVmZjVkYTk1NTA4Y2VjMDBlOGM0YTAxMGRjMDBiMDZlZjFkNzRiZmIwYTAxYWY3ZWQ3YjEiLCJhcHBJZCI6InkydHNxNWwwMmIiLCJyZWdpb25zIjpbInNlYTEiXX0='
+    }
 })
 
